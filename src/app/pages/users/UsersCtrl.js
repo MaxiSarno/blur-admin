c7f810296cf32e3f7ad78b54c80c6b2f0a141d09
Maxi Sarno <maximiliano.sarno@avantrip.com>
2017-10-30 01:14:11 -0300

    tabla editable para usuarios

--- src/app/pages/users/UsersCtrl.js
+++ src/app/pages/users/UsersCtrl.js
@@ -9,9 +9,164 @@
       .controller('UsersCtrl', UsersCtrl);
 
   /** @ngInject */
-  function UsersCtrl($scope, $http, samService) {
-    
+  function UsersCtrl($scope, $http, samService) {    
 
-  }
+    $scope.smartTableData = [
+      {
+        id: 1,
+        firstName: 'Mark',
+        lastName: 'Otto',
+        username: '@mdo',
+        email: 'mdo@gmail.com',
+        age: '28'
+      },
+      {
+        id: 2,
+        firstName: 'Jacob',
+        lastName: 'Thornton',
+        username: '@fat',
+        email: 'fat@yandex.ru',
+        age: '45'
+      },
+      {
+        id: 3,
+        firstName: 'Larry',
+        lastName: 'Bird',
+        username: '@twitter',
+        email: 'twitter@outlook.com',
+        age: '18'
+      },
+      {
+        id: 4,
+        firstName: 'John',
+        lastName: 'Snow',
+        username: '@snow',
+        email: 'snow@gmail.com',
+        age: '20'
+      },
+      {
+        id: 5,
+        firstName: 'Jack',
+        lastName: 'Sparrow',
+        username: '@jack',
+        email: 'jack@yandex.ru',
+        age: '30'
+      }
+    ];
+
+    $scope.editableTableData = $scope.smartTableData.slice(0, 36);
+
+    $scope.users = [
+      {
+      	username: 'maxisar@gmail.com',
+      	password: 'tuvieja',
+      	description: 'Maxi Sarno',
+      	role: 'ADMIN'
+      },
+      {
+      	username: 'mfgc87@gmail.com',
+      	password: 'suvieja',
+      	description: 'flor Gimenez',
+      	role: 'USER'
+      },
+      {
+        "id": 1,
+        "name": "Esther Vang",
+        "status": 4,
+        "group": 3
+      },
+      {
+        "id": 2,
+        "name": "Leah Freeman",
+        "status": 3,
+        "group": 1
+      },
+      {
+        "id": 3,
+        "name": "Mathews Simpson",
+        "status": 3,
+        "group": 2
+      },
+      {
+        "id": 4,
+        "name": "Buckley Hopkins",
+        "group": 4
+      },
+      {
+        "id": 5,
+        "name": "Buckley Schwartz",
+        "status": 1,
+        "group": 1
+      },
+      {
+        "id": 6,
+        "name": "Mathews Hopkins",
+        "status": 4,
+        "group": 2
+      },
+      {
+        "id": 7,
+        "name": "Leah Vang",
+        "status": 4,
+        "group": 1
+      },
+      {
+        "id": 8,
+        "name": "Vang Schwartz",
+        "status": 4,
+        "group": 2
+      },
+      {
+        "id": 9,
+        "name": "Hopkin Esther",
+        "status": 1,
+        "group": 2
+      },
+      {
+        "id": 10,
+        "name": "Mathews Schwartz",
+        "status": 1,
+        "group": 3
+      }
+    ];
+
+    $scope.statuses = [
+      {value: 1, text: 'Good'},
+      {value: 2, text: 'Awesome'},
+      {value: 3, text: 'Excellent'},
+    ];
+
+    $scope.roles = [ 'USER', 'ADMIN'];
+
+    $scope.showRole = function(user) {
+      if(user.group && $scope.groups.length) {
+        var selected = $filter('filter')($scope.groups, {id: user.group});
+        return selected.length ? selected[0].text : 'Not set';
+      } else return 'Not set'
+    };
+
+    $scope.showStatus = function(user) {
+      var selected = [];
+      if(user.status) {
+        selected = $filter('filter')($scope.statuses, {value: user.status});
+      }
+      return selected.length ? selected[0].text : 'Not set';
+    };
+
+
+    $scope.removeUser = function(index) {
+      $scope.users.splice(index, 1);
+    };
+
+    $scope.addUser = function() {
+      $scope.inserted = {
+        id: $scope.users.length+1,
+        name: '',
+        status: null,
+        group: null
+      };
+      $scope.users.push($scope.inserted);
+    };
+  } 
 
 })();
--- src/app/pages/users/users.module.js
+++ src/app/pages/users/users.module.js
@@ -11,25 +11,6 @@
   /** @ngInject */
   function routeConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-        /*.state('users', {
-          url: '/users',
-          template : '<ui-view  autoscroll="true" autoscroll-body-top></ui-view>',
-          abstract: true,
-          title: 'usuarios',
-          sidebarMeta: {
-            icon: 'ion-ios-flask-outline',
-            order: 100,
-          },
-        }).state('table', {
-          url: '/table',
-          templateUrl: 'app/pages/users/users.html',
-          title: 'tabla',
-          controller: 'UsersCtrl',
-          controllerAs: 'vm',
-          sidebarMeta: {
-            order: 200,
-          },
-        })*/
         .state('users', {
           url: '/users',
           templateUrl: 'app/pages/users/users.html',
@@ -41,7 +22,6 @@
             icon: 'ion-android-people'
           },
         });
-    /*$urlRouterProvider.when('/users');*/
   }
 
 })();
