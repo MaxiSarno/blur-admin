2bb100977249bc65bc3841b9526b6d9a437eb7af
Maxi Sarno <maximiliano.sarno@avantrip.com>
2017-10-30 10:00:03 -0300

    fies menores a la pantalla de usuarios

--- src/app/pages/users/UsersCtrl.js
+++ src/app/pages/users/UsersCtrl.js
@@ -11,51 +11,6 @@
   /** @ngInject */
   function UsersCtrl($scope, $http, samService) {    
 
-    $scope.smartTableData = [
-      {
-        id: 1,
-        firstName: 'Mark',
-        lastName: 'Otto',
-        username: '@mdo',
-        email: 'mdo@gmail.com',
-        age: '28'
-      },
-      {
-        id: 2,
-        firstName: 'Jacob',
-        lastName: 'Thornton',
-        username: '@fat',
-        email: 'fat@yandex.ru',
-        age: '45'
-      },
-      {
-        id: 3,
-        firstName: 'Larry',
-        lastName: 'Bird',
-        username: '@twitter',
-        email: 'twitter@outlook.com',
-        age: '18'
-      },
-      {
-        id: 4,
-        firstName: 'John',
-        lastName: 'Snow',
-        username: '@snow',
-        email: 'snow@gmail.com',
-        age: '20'
-      },
-      {
-        id: 5,
-        firstName: 'Jack',
-        lastName: 'Sparrow',
-        username: '@jack',
-        email: 'jack@yandex.ru',
-        age: '30'
-      }
-    ];
-
-    $scope.editableTableData = $scope.smartTableData.slice(0, 36);
-
     $scope.users = [
       {
       	username: 'maxisar@gmail.com',
@@ -129,30 +84,6 @@
         "group": 3
       }
     ];
-
-    $scope.statuses = [
-      {value: 1, text: 'Good'},
-      {value: 2, text: 'Awesome'},
-      {value: 3, text: 'Excellent'},
-    ];
-
-    $scope.roles = [ 'USER', 'ADMIN'];
-
-    $scope.showRole = function(user) {
-      if(user.group && $scope.groups.length) {
-        var selected = $filter('filter')($scope.groups, {id: user.group});
-        return selected.length ? selected[0].text : 'Not set';
-      } else return 'Not set'
-    };
-
-    $scope.showStatus = function(user) {
-      var selected = [];
-      if(user.status) {
-        selected = $filter('filter')($scope.statuses, {value: user.status});
-      }
-      return selected.length ? selected[0].text : 'Not set';
-    };
-
 
     $scope.removeUser = function(index) {
       $scope.users.splice(index, 1);
