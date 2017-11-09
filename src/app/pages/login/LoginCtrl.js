e5acb6942b6522cf105fa30082f71dc137840041
Maxi Sarno <maximiliano.sarno@avantrip.com>
2017-11-08 22:57:30 -0300

    oculto header footer y sidebar si no esta logueado

--- src/app/pages/login/LoginCtrl.js
+++ src/app/pages/login/LoginCtrl.js
@@ -9,10 +9,19 @@
       .controller('LoginCtrl', LoginCtrl);
 
   /** @ngInject */
-  function LoginCtrl($rootScope, $scope, $http, samService) {
+  function LoginCtrl($rootScope, $scope, $state, samService) {
     var vm = this;
-    console.log('seteando $rootScope.$isLoggedIn = true')
-    $rootScope.$isLoggedIn = true
+    console.log('seteando $rootScope.$isLoggedIn = false')
+    //$rootScope.$isLoggedIn = false
+
+    vm.login = function() {
+      $rootScope.$isLoggedIn = true
+      $state.go('dashboard')
+    }
+
+    vm.logout = function() {
+      $rootScope.$isLoggedIn = true
+    }
 
   } 
 
